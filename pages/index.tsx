ed9eb12f2cb4bbee4442dd80cfeff92e09a5b3c3
VictorMello1993 <victorsmello93@gmail.com>
2022-11-22 23:06:25 -0300

    eslint configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Text } from '../components/Text'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Text } from "../components/Text";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -69,5 +69,5 @@ export default function Home() {
     //     </a>
     //   </footer>
     // </div>
-  )
+  );
 }
