d5a6af997333ac52c343b1a425c907b9fe61170e
VictorMello1993 <victorsmello93@gmail.com>
2022-11-23 22:23:42 -0300

    Migração dos componentes implementados no Gatsby

--- src/pages/course/{MarkdownRemark.fields__slug}.tsx
+++ src/pages/course/{MarkdownRemark.fields__slug}.tsx
@@ -40,3 +40,29 @@
 //     }
 //   }
 // `;
+
+import type { GetServerSideProps } from "next";
+
+export type TesteProps = {
+  items: string[];
+};
+
+export default function Course({ items }: TesteProps) {
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<TesteProps> = async () => {
+  const items = Array.from({ length: 10 }, () => new Date().toLocaleTimeString());
+
+  return {
+    props: {
+      items,
+    },
+  };
+};
