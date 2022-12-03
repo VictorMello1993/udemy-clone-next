bd19ef971f238558375ae3081626212c884bdfff
VictorMello1993 <victorsmello93@gmail.com>
2022-12-03 16:06:04 -0300

    Update to deploy in Netlify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -72,7 +72,7 @@ export const getStaticProps: GetStaticProps<HomeProps> = async () => {
         slug,
       },
     }: any) => ({
-      image: `"https://webservices.jumpingcrab.com"${image}`,
+      image: `https://webservices.jumpingcrab.com${image}`,
       description,
       instructorName,
       ratingClassification,
