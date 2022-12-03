09f662ba6cfa17e7572e2232636319b735e1ae6c
VictorMello1993 <victorsmello93@gmail.com>
2022-12-03 15:35:34 -0300

    Update to deploy in Netlify

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: process.env.URI_STRAPI,
+        pathname: "/uploads/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
